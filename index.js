2255380b09e7725471f6cb039bc5199b9a50945c
ketantb <kbhatkhore777@gmail.com>
2023-03-03 01:40:53 +0530

    changed model name and added "/" routing

--- index.js
+++ index.js
@@ -1,64 +1,58 @@
 const express = require("express")
-const fs = require("fs")
 const cors = require("cors")
-const { response } = require("express")
-const fileUpload = require("express-fileupload")
-// const {User} = require("./models/user.js")
-const {Post} = require("./models/post.js")
+const app = express()
+const {Post} = require("./models/postModel.js")
 require('dotenv').config();
 const mongoose = require('mongoose')
-const path = require("path")
 const key = process.env.key
 
-const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(fileUpload())
+
 const PORT = process.env.PORT || 8081 
 
+//Connection to DB =>
 mongoose.set('strictQuery', true);
 mongoose.connect(key, (err) => {
-    // if(err){
-    //     console.log("mongodb connection failed")
-    // }
-    // else{
-    //     console.log("connected to mongodb")
-    // }
+    if(err){
+        console.log("mongodb connection failed")
+    }
+    else{
+        console.log("connected to mongodb")
+    }
 })
 
-// const data = fs.readFileSync("./templates/product.json", "utf-8")
+app.get("/", async (request, response) => {
+    try{
+        response.json(await Post.find())
+    }
+    catch(err){
+        response.sendStatus(500).json({message: err})
+    }
+})
 
 app.get("/all", async (request, response) => {
-    response.json(await Post.find())
+    try{
+        response.json(await Post.find())
+    }
+    catch(err){
+        response.sendStatus(500).json({message: err})
+    }
 })
 
-// app.get("/img", async (request, response) => {
-//     // response.sendFile(path.join(__dirname, `./uploads/${req.params.fileName}`))
-// })
-
 app.post("/post", async (request, response) => {
-    const {date, description, id, likes, location, name} = request.body
-    console.log({date, description, id, likes, location, name})
-    const {image} = request.files
-    image.mv("./uploads/"+image.name, (err) => {
-        if(err){
-            response.json({message: err})
-        }
-        else{
-            response.json({message: "uploaded Successfully"})
-            console.log("uploaded Successfully")
-        }
-    })
+    const {date, description, id, likes, location, imgUrl, author} = request.body
+    console.log({date, description, id, likes, location, imgUrl, author})
     const post = new Post({
-       ...{ date, description, id, likes, location, name },
-       image: id
+       ...{ date, description, id, likes, location, imgUrl, author }
     })
     try{
         console.log(post)
-    const resp = await post.save()
+        const resp = await post.save()
+        response.json(resp)
     }
     catch(err){
-        console.log(err)
+        response.sendStatus(500).json({message: err})
     }
 })
 
